2aef6c5040704df8b089308261d2a25694a70cb9
hamisicodes <newhamisi@gmail.com>
2020-03-28 15:43:40 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
+import { ComponentDetailComponent } from './component-detail/component-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteComponent,
+    ComponentDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component-detail/component-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-detail',
+  templateUrl: './component-detail.component.html',
+  styleUrls: ['./component-detail.component.css']
+})
+export class ComponentDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
