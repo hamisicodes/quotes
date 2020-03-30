21df637e03f92ed4116916ec51bf4f8b8c81aeb4
hamisicodes <newhamisi@gmail.com>
2020-03-30 23:30:29 +0300

    perform input property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
-import { ComponentDetailComponent } from './component-detail/component-detail.component';
 import { FormsModule } from '@angular/forms';
 import { TimeAgoPipe } from './time-ago.pipe';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
@@ -16,7 +15,6 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
     AppComponent,
     QuoteFormComponent,
     QuoteComponent,
-    ComponentDetailComponent,
     TimeAgoPipe,
     QuoteDetailComponent,
     
